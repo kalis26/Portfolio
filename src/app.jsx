ca5a798d328aa13aaa0fa04033d771f103fe9503
Amine Mustapha Rachid <AmineRA2006@gmail.com>
2025-08-13 11:31:50 +0100

    changed the image for mobile view

--- src/app.jsx
+++ src/app.jsx
@@ -3,15 +3,54 @@ import About from './components/about.jsx';
 import Navbar from './components/navbar.jsx';
 import Experience from './components/experience.jsx'
 import Skills from './components/skills.jsx'
+import Footer from './components/footer.jsx'
 
 function SetMainSection() {
     return (
-        <div className="p-10 flex flex-col gap-16 lg:gap-36">
-            <img src="/images/panel.png" alt="Logo" className="mx-auto rounded-2xl" />
-            <About />
-            <Skills />
-            <Experience />
-        </div>
+        <>
+            <div className="p-5 md:p-10 flex flex-col gap-16 lg:gap-36">
+                <div className='rounded-lg pt-6 pl-6 pr-6 md:rounded-2xl md:pt-16 md:pl-6 md:pr-6 bg-[linear-gradient(red,transparent),linear-gradient(to_top_left,lime,transparent),linear-gradient(to_top_right,blue,transparent)] bg-[length:auto] bg-blend-screen relative'>
+                    <div className='flex flex-col items-center justify-center pb-28 md:hidden'>
+                        <h1 className='text-white font-[Geist-Bold] text-6xl text-center'>Amine Rachid</h1>
+                        <p className='text-white/70 font-[Geist-Light] text-2xl text-center'>Computer Science Student & Junior Dev</p>
+                    </div>
+                    <img src='/images/panel2.png' alt='Panel' className='hidden md:block mx-auto'></img>
+                    <img src='/images/panelmobile.png' alt='PanelMobile' className='md:hidden'></img>
+                    <div className="hidden absolute bottom-10 md:right-24 md:flex flex-row gap-3">
+                        <div className='rounded-lg border border-white/20 hover:border-white transition-all duration-200'>
+                            <button className="m-1 bg-white text-black px-6 py-3 rounded-md font-[Geist-Medium] text-sm transition-all duration-300 flex items-center justify-center">
+                                View My Work
+                            </button> 
+                        </div>
+                        <div className='rounded-lg border border-white/20 hover:border-white transition-all duration-200'>
+                            <button className="m-1 bg-black text-white px-6 py-3 rounded-lg font-[Geist-Medium] text-sm flex items-center justify-center">
+                                Contact Me
+                            </button>
+                        </div>
+                    </div>
+                    <div className='w-full h-56 md:hidden absolute bg-gradient-to-b from-white/0 to-black bottom-0 left-0 pointer-events-none z-20'>
+                    
+                    </div>
+                    <div className='p-4 flex flex-col gap-3 md:hidden z-30 absolute bottom-8 left-0 right-0'>
+                        <div className='flex w-full rounded-lg border border-white/20 hover:border-white transition-all duration-200'>
+                            <button className="flex-1 m-1 bg-white text-black px-6 py-3 rounded-md font-[Geist-Medium] text-sm transition-all duration-300 flex items-center justify-center">
+                                View My Work
+                            </button> 
+                        </div>
+                        <div className='flex w-full rounded-lg border border-white/20 hover:border-white transition-all duration-200'>
+                            <button className="flex-1 m-1 bg-black text-white px-6 py-3 rounded-md font-[Geist-Medium] text-sm transition-all duration-300 flex items-center justify-center">
+                                Contact Me
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <About />
+                <Skills />
+                <Experience />
+                
+            </div>
+            <Footer />
+        </>
     );
 }
 
---
+++ src/components/footer.jsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+    return (
+        <div className="w-full bg-black flex flex-col p-6 font-[Geist-Regular] border-t-2 border-solid border-white/10">
+            <div>
+                <h1 className="text-xl font-[Geist-Bold] text-white/90 pb-2">Contact</h1>
+                <p className="text-base text-white/50">E-mail: aminera2006@gmail.com</p>
+            </div>
+            <div className="flex flex-col md:flex-row justify-between items-start md:items-center gap-4 md:gap-0">
+                <p className="text-base text-white/50">Telephone: +213 798 19 92 08</p>
+                <p className="text-base text-white/50">&copy; 2025 Made by Amine Mustapha Rachid</p>
+            </div>
+        </div>
+    );
+}
