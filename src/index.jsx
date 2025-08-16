9483c19b03541a2936b29c459ee5276817e1ed36
Amine Mustapha Rachid <AmineRA2006@gmail.com>
2025-08-16 12:19:08 +0100

    Added vercel analytics

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react"
 import './index.css';
 import App from './app.jsx';
 
@@ -9,5 +10,6 @@ root.render(
   <StrictMode>
     <App />
     <SpeedInsights />
+    <Analytics />
   </StrictMode>
 );
